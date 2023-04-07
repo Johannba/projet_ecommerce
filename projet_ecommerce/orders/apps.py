e12f374f2b8a6411677fa3d3b618916c768bb518
Johannba <johannbacha@gmail.com>
2023-04-07 10:39:25 +0200

    création app

--- projet_ecommerce/orders/apps.py
+++ projet_ecommerce/orders/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PagesConfig(AppConfig):
+class OrdersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "projet_ecommerce.pages"
+    name = "orders"
